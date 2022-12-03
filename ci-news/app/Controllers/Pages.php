15b9c589ebe22dd1568c1217625ab43906c745be
g_rabbany <golamrabbany10@gmailcom>
2022-12-03 13:39:18 +0600

    cl-news

---
+++ ci-news/app/Controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    public function view($page = 'home')
+    {
+    }
+}
